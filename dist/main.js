89da5261a9d0293d8b96a32d67403c8c8d04f09b
Alex <97993854+AlexFariaa@users.noreply.github.com>
2022-08-06 14:36:14 -0300

    feat(main): swagger

--- dist/main.js
+++ dist/main.js
@@ -8,11 +8,11 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     const config = new swagger_1.DocumentBuilder()
-        .setTitle('PizzaFresh')
-        .setDescription('Aplicação para gestão das mesas de uma pizzaria')
+        .setTitle('Coluna Reta')
+        .setDescription('A fazer')
         .setVersion('1.0.0')
         .addTag('status')
-        .addTag('table')
+        .addTag('users')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, document);
