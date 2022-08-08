d7f0cbb04ca12162ecfcaf2b7ec03de63b01c698
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2022-08-08 19:58:14 -0300

    feat: structuring (studant.service)

--- src/studants/studants.service.ts
+++ src/studants/studants.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utility/handle-error.utility';
 import { CreateStudantDto } from './dto/create-studant.dto';
 import { UpdateStudantDto } from './dto/update-studant.dto';
+import { Studant } from './entities/studant.entity';
 
 @Injectable()
 export class StudantsService {
-  create(createStudantDto: CreateStudantDto) {
-    return 'This action adds a new studant';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createStudantDto: CreateStudantDto): Promise<Studant> {
+    const data: Prisma.StudantCreateInput = {
+
+    }
+    return await this.prisma.studant.create({ data }).catch(handleError);
   }
 
-  findAll() {
-    return `This action returns all studants`;
+  findAll(): Promise<Studant[]> {
+    return this.prisma.studant.findMany();
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} studant`;
+  async findById(id: string): Promise<Studant> {
+    const record = await this.prisma.studant.findUnique({ where: { id } });
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID '${id}' não encontrado`);
+    }
+    return record;
   }
 
-  update(id: string, updateStudantDto: UpdateStudantDto) {
-    return `This action updates a #${id} studant`;
+  findOne(id: string): Promise<Studant> {
+    return this.findById(id);
   }
 
-  delete(id: string) {
-    return `This action removes a #${id} studant`;
+  async update(id: string, dto: UpdateStudantDto): Promise<Studant> {
+    await this.findById(id);
+
+    const data: Partial<Prisma.StudantCreateInput> = {
+
+    }
+    return this.prisma.studant.update({
+      where: { id },
+      data
+    }).catch(handleError);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+    await this.prisma.studant.delete({ where: { id } });
   }
 }
