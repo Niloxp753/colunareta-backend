2da70a0f3674b59299892e3dcd00ad4e6f8d9b11
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2022-08-08 18:23:03 -0300

    feat: implement PrismaModule in (studants.module)

--- src/studants/studants.module.ts
+++ src/studants/studants.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StudantsService } from './studants.service';
 import { StudantsController } from './studants.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+  ],
   controllers: [StudantsController],
   providers: [StudantsService]
 })
