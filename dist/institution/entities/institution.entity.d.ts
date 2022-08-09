11e330d1d40a0860bb35cd75013e20307d0ef203
Niloxp753 <niloxp.753@gmail.com>
2022-08-08 21:58:55 -0300

    fix(institution): update entities

--- dist/institution/entities/institution.entity.d.ts
+++ dist/institution/entities/institution.entity.d.ts
@@ -1,2 +1,16 @@
+import { User } from 'src/user/entities/user.entity';
 export declare class Institution {
+    id: string;
+    nome: string;
+    telefone: string;
+    cep: string;
+    numero: string;
+    logradouro: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
+    complemento: string;
+    usuarios: User[];
+    createdAt?: Date;
+    updatedAt?: Date;
 }
--- src/institution/entities/institution.entity.ts
+++ src/institution/entities/institution.entity.ts
@@ -1,1 +1,20 @@
-export class Institution {}
+import { User } from 'src/user/entities/user.entity';
+
+export class Institution {
+  id: string;
+  nome: string;
+  telefone: string;
+  cep: string;
+  numero: string;
+  logradouro: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  complemento: string;
+
+  usuarios: User[];
+  // alunos: Studant[];
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
