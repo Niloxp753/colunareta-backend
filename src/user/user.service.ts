6035881fb98074e57b2ee63bd61022d64b7269ff
Alex <97993854+AlexFariaa@users.noreply.github.com>
2022-08-09 20:39:09 -0300

    fix(user): Update user service handleError

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,6 +5,7 @@ import {
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utility/handle-error.utility';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -25,14 +26,14 @@ export class UserService {
   findAll(): Promise<User[]> {
     return this.prisma.user.findMany({
       select: this.userSelect,
-    });
+    }).catch(handleError);;
   }
 
   async findById(id: string): Promise<User> {
     const record = await this.prisma.user.findUnique({
       where: { id },
       select: this.userSelect,
-    });
+    }).catch(handleError);;
     if (!record) {
       throw new NotFoundException(`Registro com o ID '${id}' não encontrado`);
     }
@@ -40,14 +41,14 @@ export class UserService {
   }
 
   async findOne(id: string): Promise<User> {
-    return this.findById(id);
+    return this.findById(id).catch(handleError);;
   }
 
   async create(dto: CreateUserDto): Promise<User> {
 
     const data: User = { ...dto };
 
-    return this.prisma.user.create({ data });
+    return this.prisma.user.create({ data }).catch(handleError);;
   }
 
   async update(id: string, dto: UpdateUserDto): Promise<User> {
@@ -61,25 +62,11 @@ export class UserService {
         data,
         select: this.userSelect,
       })
-      .catch(this.handleError);
+      .catch(handleError);
   }
 
   async delete(id: string) {
     await this.findById(id);
-    await this.prisma.user.delete({ where: { id } });
-  }
-
-  handleError(error: Error): undefined {
-    //visualização de erro
-    const errorLines = error.message?.split('\n');
-    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
-
-    if (!lastErrorLine) {
-      console.error(error);
-    }
-
-    throw new UnprocessableEntityException(
-      lastErrorLine || 'Algum erro ocorreu ao executar a operação',
-    );
+    await this.prisma.user.delete({ where: { id } }).catch(handleError);;
   }
 }
