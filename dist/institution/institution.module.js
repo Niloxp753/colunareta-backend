2507fb7750e37f2ed8421cb45fa197862a7df0b1
Niloxp753 <niloxp.753@gmail.com>
2022-08-08 22:48:13 -0300

    fix(institution): update institution.module

--- dist/institution/institution.module.js
+++ dist/institution/institution.module.js
@@ -10,12 +10,14 @@ exports.InstitutionModule = void 0;
 const common_1 = require("@nestjs/common");
 const institution_service_1 = require("./institution.service");
 const institution_controller_1 = require("./institution.controller");
+const prisma_module_1 = require("../prisma/prisma.module");
 let InstitutionModule = class InstitutionModule {
 };
 InstitutionModule = __decorate([
     (0, common_1.Module)({
+        imports: [prisma_module_1.PrismaModule],
         controllers: [institution_controller_1.InstitutionController],
-        providers: [institution_service_1.InstitutionService]
+        providers: [institution_service_1.InstitutionService],
     })
 ], InstitutionModule);
 exports.InstitutionModule = InstitutionModule;
--- src/institution/institution.module.ts
+++ src/institution/institution.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InstitutionService } from './institution.service';
 import { InstitutionController } from './institution.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [InstitutionController],
-  providers: [InstitutionService]
+  providers: [InstitutionService],
 })
 export class InstitutionModule {}
