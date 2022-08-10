538c70f5bfe910d1094c479aeff6c952d5d8e3e1
Niloxp753 <niloxp.753@gmail.com>
2022-08-09 23:51:12 -0300

    fix(institution): update controller and service

--- src/institution/institution.controller.ts
+++ src/institution/institution.controller.ts
@@ -6,31 +6,38 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { InstitutionService } from './institution.service';
 import { CreateInstitutionDto } from './dto/create-institution.dto';
 import { UpdateInstitutionDto } from './dto/update-institution.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
 
 @ApiTags('institutions')
 @Controller('institutions')
 export class InstitutionController {
   constructor(private readonly institutionService: InstitutionService) {}
 
-  @Post('create')
+  @Post()
   @ApiOperation({
     summary: 'Cria uma nova instituição',
   })
-  create(@Body() createInstitutionDto: CreateInstitutionDto) {
-    return this.institutionService.create(createInstitutionDto);
+  create(
+    @LoggedUser() user: User,
+    @Body() createInstitutionDto: CreateInstitutionDto,
+  ) {
+    return this.institutionService.create(user.id, createInstitutionDto);
   }
 
-  @Get('find-all')
+  @Get()
   @ApiOperation({
-    summary: 'visualiza todas as instituições',
+    summary: 'Lista todas as instituições',
   })
-  findAll() {
-    return this.institutionService.findAll();
+  findAll(@LoggedUser() user: User) {
+    return this.institutionService.findAll(user);
   }
 
   @Get(':id')
@@ -46,13 +53,15 @@ export class InstitutionController {
     summary: 'Atualiza uma instituição pelo ID',
   })
   update(
+    @LoggedUser() user: User,
     @Param('id') id: string,
     @Body() updateInstitutionDto: UpdateInstitutionDto,
   ) {
-    return this.institutionService.update(id, updateInstitutionDto);
+    return this.institutionService.update(user.id, id, updateInstitutionDto);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({
     summary: 'Deleta uma instituição pelo ID',
   })
--- src/institution/institution.service.ts
+++ src/institution/institution.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'src/user/entities/user.entity';
 import { handleError } from 'src/utility/handle-error.utility';
 import { CreateInstitutionDto } from './dto/create-institution.dto';
 import { UpdateInstitutionDto } from './dto/update-institution.dto';
@@ -8,7 +9,7 @@ import { UpdateInstitutionDto } from './dto/update-institution.dto';
 export class InstitutionService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(dto: CreateInstitutionDto) {
+  async create(userId: string, dto: CreateInstitutionDto) {
     if (dto.usuariosId) {
       return await this.prisma.institution
         .create({
@@ -36,19 +37,154 @@ export class InstitutionService {
     }
   }
 
-  findAll() {
-    return `This action returns all institution`;
+  async findAll(user: User) {
+    const institutionList = await this.prisma.institution.findMany({
+      where: {
+        usuarios: {
+          every: {
+            id: user.id,
+          },
+        },
+      },
+      select: {
+        id: true,
+        nome: true,
+        telefone: true,
+        cep: true,
+        cidade: true,
+        estado: true,
+        logradouro: true,
+        bairro: true,
+        numero: true,
+        complemento: true,
+        alunos: true,
+      },
+    });
+
+    if (institutionList.length === 0) {
+      throw new NotFoundException(
+        'Não existe insituições cadastrados para esse usuário.',
+      );
+    }
+    return institutionList;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} institution`;
+  async findById(id: string) {
+    const record = await this.prisma.institution.findUnique({
+      where: { id },
+      include: {
+        alunos: true,
+      },
+    });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID '${id}' não encontrado.`);
+    }
+
+    return record;
   }
 
-  update(id: string, dto: UpdateInstitutionDto) {
-    return `This action updates a #${id} institution`;
+  async findOne(id: string) {
+    await this.findById(id);
+
+    return await this.prisma.institution.findUnique({
+      where: { id },
+      select: {
+        nome: true,
+        telefone: true,
+        cep: true,
+        cidade: true,
+        estado: true,
+        logradouro: true,
+        bairro: true,
+        numero: true,
+        complemento: true,
+        alunos: {
+          select: {
+            id: true,
+            nome: true,
+            idade: true,
+            telefone: true,
+            url_image: true,
+            cep: true,
+            cidade: true,
+            estado: true,
+            logradouro: true,
+            bairro: true,
+            consultas: true,
+          },
+        },
+      },
+    });
   }
 
-  delete(id: string) {
-    return `This action removes a #${id} institution`;
+  async update(userId: string, id: string, dto: UpdateInstitutionDto) {
+    const InstitutionUser = await this.findById(id);
+
+    if (dto.alunosId) {
+      let existAluno = false;
+      InstitutionUser.alunos.map((aluno) => {
+        if (aluno.id == dto.alunosId) {
+          existAluno = true;
+        }
+      });
+
+      if (existAluno) {
+        return this.prisma.institution
+          .update({
+            where: { id: id },
+            data: {
+              nome: dto.nome,
+              telefone: dto.telefone,
+              cep: dto.cep,
+              cidade: dto.cidade,
+              estado: dto.estado,
+              logradouro: dto.logradouro,
+              bairro: dto.bairro,
+              numero: dto.numero,
+              complemento: dto.complemento,
+              alunos: {
+                disconnect: {
+                  id: dto.alunosId,
+                },
+              },
+            },
+            include: {
+              alunos: true,
+            },
+          })
+          .catch(handleError);
+      } else {
+        return this.prisma.institution
+          .update({
+            where: { id: id },
+            data: {
+              nome: dto.nome,
+              telefone: dto.telefone,
+              cep: dto.cep,
+              cidade: dto.cidade,
+              estado: dto.estado,
+              logradouro: dto.logradouro,
+              bairro: dto.bairro,
+              numero: dto.numero,
+              complemento: dto.complemento,
+              alunos: {
+                connect: {
+                  id: dto.alunosId,
+                },
+              },
+            },
+            include: {
+              alunos: true,
+            },
+          })
+          .catch(handleError);
+      }
+    }
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+    await this.prisma.institution.delete({ where: { id } });
   }
 }
