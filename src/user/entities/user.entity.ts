8f6019d3e58c0045993bb78f3962e1f620ee2774
Alex <97993854+AlexFariaa@users.noreply.github.com>
2022-08-10 21:01:01 -0300

    fix(entity): update user.entity `instituicoes`

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,13 @@
+import { Institution } from "@prisma/client";
+
 export class User {
   id?: string;
   nome: string;
   email: string;
   cargo: string;
+  senha: string;
   createdAt?: Date;
   updatedAt?: Date;
+
+  instituicoes?: Institution[]
 }
