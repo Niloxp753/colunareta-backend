8bb67ec2e7d0642e070d1234b1d0c1060eac65ca
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2022-08-10 22:50:07 -0300

    fix: correction code in (studants.service)

--- src/studants/studants.service.ts
+++ src/studants/studants.service.ts
@@ -31,7 +31,7 @@ export class StudantsService {
   findAll(): Promise<Studant[]> {
     return this.prisma.studant.findMany({
       select: this.studantsSelect,
-    }).catch(handleError);;
+    }).catch(handleError);
   }
 
   async findById(id: string): Promise<Studant> {
