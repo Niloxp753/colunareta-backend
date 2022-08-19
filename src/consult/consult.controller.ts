43501141740eed891b95cb9e44b8cffdef8cf3aa
Niloxp753 <niloxp.753@gmail.com>
2022-08-19 00:36:42 -0300

    feat(consult): implementing the roles in the endpoint

--- src/consult/consult.controller.ts
+++ src/consult/consult.controller.ts
@@ -12,6 +12,9 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Role } from 'src/auth/models/role.enum';
 import { ConsultService } from './consult.service';
 import { CreateConsultDto } from './dto/create-consult.dto';
 import { UpdateConsultDto } from './dto/update-consult.dto';
@@ -23,6 +26,8 @@ import { UpdateConsultDto } from './dto/update-consult.dto';
 export class ConsultController {
   constructor(private readonly consultService: ConsultService) {}
 
+  @Roles(Role.ADMIN, Role.BACKOFFICE, Role.CAMPO)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Post()
   @ApiOperation({
     summary: 'Cria uma nova consulta',
@@ -30,15 +35,18 @@ export class ConsultController {
   create(@Body() createConsultDto: CreateConsultDto) {
     return this.consultService.create(createConsultDto);
   }
-
+  @Roles(Role.ADMIN, Role.BACKOFFICE)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Get('find-all')
   @ApiOperation({
-    summary: 'Lista todas as consultas cadastrados',
+    summary: 'Lista todas as consultas cadastradas',
   })
   findAll() {
     return this.consultService.findAll();
   }
 
+  @Roles(Role.ADMIN, Role.BACKOFFICE, Role.CAMPO)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Get(':id')
   @ApiOperation({
     summary: 'Visualiza uma consulta pelo ID',
@@ -47,6 +55,8 @@ export class ConsultController {
     return this.consultService.findById(id);
   }
 
+  @Roles(Role.ADMIN, Role.BACKOFFICE, Role.CAMPO)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Patch(':id')
   @ApiOperation({
     summary: 'Atualiza uma consulta pelo ID',
@@ -55,6 +65,8 @@ export class ConsultController {
     return this.consultService.update(id, dto);
   }
 
+  @Roles(Role.ADMIN, Role.BACKOFFICE, Role.CAMPO)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({
