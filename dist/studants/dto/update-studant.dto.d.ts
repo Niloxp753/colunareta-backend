75c32dda2a7b251f152d288bd8f318b3d407eed8
Niloxp753 <niloxp.753@gmail.com>
2022-08-29 00:04:42 -0300

    fix(dist): delete

--- dist/studants/dto/update-studant.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateStudantDto } from './create-studant.dto';
-declare const UpdateStudantDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateStudantDto>>;
-export declare class UpdateStudantDto extends UpdateStudantDto_base {
-}
-export {};
--- dist/studants/entities/studant.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Studant = void 0;
-class Studant {
-}
-exports.Studant = Studant;
-//# sourceMappingURL=studant.entity.js.map
